82f8c240cca82a5266d332886427f2f5b7071efb
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-16 16:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function removeFromCart(item) {
            console.log('cart =', cart );
            var icart = [...cart.slice(0, i), ...cart.slice(i+1)];
            console.log('icart =', icart );
-           cart = icart; 
+           cart = icart;
            return (cart);
           }
           else {
